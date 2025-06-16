dadd0f16296452bf899f060bf752cd345f39df01
jiksrun <dzikrayya@gmail.com>
2025-06-16 10:55:27 +0700

    set default colorscheme to catppuccin

--- init.lua
+++ init.lua
@@ -2,10 +2,11 @@
 require("config.lazy")
 
 -- vim.cmd("colorscheme kanagawa")
-vim.cmd("colorscheme tokyonight-night")
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#A020F0" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFD700" })
+-- vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme catppuccin-macchiato")
+-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "LineNr", { fg = "#A020F0" })
+-- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFD700" })
 
 -- enable copy and paste with wayland clipboard
 vim.opt.clipboard = ""
