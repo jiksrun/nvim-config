1be2a8ce28745bfae23e388b7fce81457bc9dcc6
jiksrun <dzikrayya@gmail.com>
2025-07-11 02:57:10 +0700

    ini apa ya lupa

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set('n', "<C-\\>", ":ToggleTerm<CR>", { desc = "Toggle Terminal" })
+-- vim.keymap.set('n', "<C-\\>", ":ToggleTerm<CR>", { desc = "Toggle Terminal" })
 -- vim.keymap.set('n', "cd", ":cd %:p:h<CR>", { desc = "Set CWD to current file's directory" }) -- ini udah ga perlu (udah nambah di autocmd)
 -- vim.keymap.set("i", "<C-h>", function()
 --   require("blink.cmp.signature.trigger").hide()
