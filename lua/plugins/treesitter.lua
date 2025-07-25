cb1f7ce59294807fe35396e57c062a48a3cf9da7
jiksrun <dzikrayya@gmail.com>
2025-07-25 11:00:02 +0700

    plugins for web development

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "c",
+        "cpp",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+}
