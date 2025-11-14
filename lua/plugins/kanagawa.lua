0d029a23fde0dc997b1ccdfec86d52ad87d9fbce
jiksrun <dzikrayya@gmail.com>
2025-11-14 21:32:08 +0700

    kanagawa: disable undercurl, disable italic for keyword, make background to be transparent.

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -5,13 +5,13 @@ return {
             -- Default options:
             require('kanagawa').setup({
                 compile = false,             -- enable compiling the colorscheme
-                undercurl = true,            -- enable undercurls
+                undercurl = false,            -- enable undercurls
                 commentStyle = { italic = true },
                 functionStyle = {},
-                keywordStyle = { italic = true},
+                keywordStyle = { italic = false },
                 statementStyle = { bold = true },
                 typeStyle = {},
-                transparent = false,         -- do not set background color
+                transparent = true,         -- do not set background color
                 dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
                 terminalColors = true,       -- define vim.g.terminal_color_{0,17}
                 colors = {                   -- add/modify theme and palette colors
