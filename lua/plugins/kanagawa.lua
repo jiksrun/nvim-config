1c1f4220dca073793fb0d948f868f3e924978be3
jiksrun <dzikrayya@gmail.com>
2025-11-14 21:16:04 +0700

    set kanagawa themes to use no glutter

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,3 +1,52 @@
 return {
-  { "https://github.com/rebelot/kanagawa.nvim" }
+    {
+        "https://github.com/rebelot/kanagawa.nvim",
+        config = function()
+            -- Default options:
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true},
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none"
+                            }
+                        }
+                    },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    local theme = colors.theme
+                    return {
+                        -- General window separators
+                        WinSeparator = { fg = theme.ui.nontext },
+
+                        -- Snacks picker / explorer borders
+                        SnacksPickerBorder    = { fg = theme.ui.nontext, bg = theme.ui.bg },
+                        SnacksPickerSeparator = { fg = theme.ui.nontext },
+                        SnacksPickerNormal    = { bg = theme.ui.bg },
+                        SnacksPickerTitle     = { fg = theme.ui.special, bold = true },
+
+                        -- For Snacks explorer specifically (depends on version)
+                        SnacksExplorerBorder = { fg = theme.ui.nontext, bg = theme.ui.bg },
+                        SnacksExplorerNormal = { bg = theme.ui.bg },
+                    }
+                end,
+                theme = "wave",              -- Load "wave" theme
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+        end
+    }
 }
