71a5aa8012b8c5644dfdf88ec13eb41b05c0aaa5
jiksrun <dzikrayya@gmail.com>
2025-11-14 20:12:51 +0700

    add highlight and indent to be true

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,35 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "css",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "c",
-        "cpp",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "java",
-      },
+        ensure_installed = {
+            "bash",
+            "html",
+            "css",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "c",
+            "cpp",
+            "query",
+            "regex",
+            "tsx",
+            "typescript",
+            "vim",
+            "yaml",
+            "java",
+        },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = true
+        },
+        indent = {
+            enable = true,
+        },
+        autotag = {
+            enable = true,
+        }
     },
 }
