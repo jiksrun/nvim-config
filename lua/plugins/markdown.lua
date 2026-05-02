7b00ef849d992296aa173dcb3a9b96df0c150f2b
jiksrun <dzikrayya@gmail.com>
2026-05-03 01:32:47 +0700

    remove markdown linter

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,24 +1,46 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = function()
-    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-    vim.fn["mkdp#util#install"]()
-  end,
-  config = function()
-    -- Browser config (optional)
-    vim.g.mkdp_browser = "zen-browser" -- or remove if causing issues
-    vim.g.mkdp_filetypes = { "markdown" }
+  {
+    "iamcco/markdown-preview.nvim",
 
-    -- Trigger FileType autocmd manually
-    vim.cmd([[do FileType]])
+    ft = { "markdown" },
 
-    -- Setup keymap only for markdown files
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "markdown",
-      callback = function()
-        vim.keymap.set("n", "<leader>cp", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Markdown Preview", buffer = true })
-      end,
-    })
-  end,
+    cmd = {
+      "MarkdownPreviewToggle",
+      "MarkdownPreview",
+      "MarkdownPreviewStop",
+    },
+
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_browser = "zen-browser"
+    end,
+
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.keymap.set(
+            "n",
+            "<leader>cp",
+            "<cmd>MarkdownPreviewToggle<CR>",
+            { buffer = true, desc = "Markdown Preview" }
+          )
+        end,
+      })
+    end,
+  },
+
+  -- disable markdownlint for markdown files
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
 }
